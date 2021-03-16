4fc8d3e42e33c49f0284d88ec296a42915cc2224
AlekxxxP1K4 <50950970+AlekxxxP1K4@users.noreply.github.com>
2021-03-16 11:48:11 +0000

    vv

--- procedures/procedures_commands.sql
+++ procedures/procedures_commands.sql
@@ -5,3 +5,12 @@ ALTER PROCEDURE PRC_INSERE_EMPREGADO COMPILE;
 --Removing procedure
 
 DROP PROCEDURE PRC_CONSULTA_EMPREGADO;
+
+--exec procedure
+exec PRC_FETCH_EMPLOYEES_DYNAMIC(pmanager_id => 103, pdepartment_id => 60)
+
+exec PRC_FETCH_EMPLOYEES_DYNAMIC(pmanager_id => 103)
+
+exec PRC_FETCH_EMPLOYEES_DYNAMIC(pdepartment_id => 60)
+
+exec PRC_FETCH_EMPLOYEES_DYNAMIC
